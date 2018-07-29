e8b513577751962b6f317e09167c6f44bf24cd35
Javier <sonico@ciencias.unam.mx>
2018-07-29 18:31:38 -0500

    Variable de entorno asignada

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ======= BASE DE DATOS ======= 
 let urlDB = process.env.NODE_ENV === 'dev' ?
 			'mongodb://localhost:27017/cafe' :
-			'mongodb://cafe-user:jav3083@ds143900.mlab.com:43900/cafe';
+			process.env.MONGO_URL;
 process.env.URL_DB = urlDB;
