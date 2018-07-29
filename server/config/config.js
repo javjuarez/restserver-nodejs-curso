9afd471a62e26498d80fbad419c6c81120e26def
Javier <sonico@ciencias.unam.mx>
2018-07-29 18:17:51 -0500

    RESTServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,11 @@
 // ======= PUERTO ======= 
 process.env.PORT = process.env.PORT || 3000;
+
+// ======= ENTORNO ======= 
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ======= BASE DE DATOS ======= 
+let urlDB = process.env.NODE_ENV === 'dev' ?
+			'mongodb://localhost:27017/cafe' :
+			'mongodb://cafe-user:jav3083@ds143900.mlab.com:43900/cafe';
+process.env.URL_DB = urlDB;
